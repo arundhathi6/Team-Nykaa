671a0b2e000592c4dfcee965bcca67c238d1477d
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-02-26 20:58:55 +0530

    payment

---
+++ components/home/payment/Success.jsx
@@ -0,0 +1,13 @@
+import "./success.css"
+
+export const Success=()=>{
+    return(
+        <>
+            <img src="https://www.clipartmax.com/png/full/179-1795386_patient-success-success-icon-png.png" alt=""/>
+    <h2>Thank you for your purchase!</h2>
+    <p>Your order id:2858851 <span id='order'></span></p>
+
+    <button onclick="">Continue Shopping</button>
+        </>
+    )
+}
