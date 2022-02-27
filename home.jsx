baa938766283d9793347609ed7beb53ddb35ae42
Tanmay Shirsat <93374610+TanmayShirsat@users.noreply.github.com>
2022-02-27 17:38:30 +0530

    Add files via upload

---
+++ home.jsx
@@ -0,0 +1,339 @@
+import "./home.css"
+
+
+export function Home(){
+    return(
+
+<div>
+
+
+<div id="singles">
+      <img
+        src="https://images-static.nykaa.com/uploads/e0aefa71-3177-458c-bd10-44ba726ca25e.jpg?tr=w-1200,cm-pad_resize"
+      />
+    </div>
+
+
+<div id="singles">
+      <img
+        src="https://images-static.nykaa.com/uploads/2bb41365-5a87-4b19-a447-5ab37b032c0e.jpg?tr=w-1200,cm-pad_resize"
+      />
+    </div>
+    <div>
+      <h2 id="spot">TOP BRANDS</h2>
+      
+      <div class="skincare" >
+        <img onclick="sign()"
+          id="minimalist" 
+          src="https://images-static.nykaa.com/uploads/92e5cf52-c068-4a78-87cf-983db9204c3b.jpg?tr=w-600,cm-pad_resize"
+        />
+  
+        <img onclick="sign()"
+          id="minimalist" 
+          src="https://images-static.nykaa.com/uploads/3df31fa9-9a20-468b-b31b-6ea541bed775.jpg?tr=w-600,cm-pad_resize"
+        />
+        <img onclick="sign()"
+          id="minimalist" 
+          src="https://images-static.nykaa.com/uploads/75f6d1fd-9a9e-4d05-88d6-aa8ba8bb5d8f.jpg?tr=w-600,cm-pad_resize"
+        />
+        <img onclick="sign()"
+          id="minimalist" 
+          src="https://images-static.nykaa.com/uploads/42085676-8e76-43c9-be46-8548410a7fb8.jpg?tr=w-600,cm-pad_resize"
+        />
+        <img onclick="sign()"
+          id="minimalist" 
+          src="https://images-static.nykaa.com/uploads/616b3c82-43b8-443c-ae7b-8db792fd2615.jpg?tr=w-600,cm-pad_resize"
+        />
+        <img onclick="sign()"
+          id="minimalist" 
+          src="https://images-static.nykaa.com/uploads/13f20df2-0d1d-49b7-b38a-221bb50de0c7.png?tr=w-600,cm-pad_resize" 
+        />
+        <img onclick="sign()"
+          id="minimalist" 
+          src="https://images-static.nykaa.com/uploads/b58d7584-6dc9-4712-9095-17bc626b2a75.gif?tr=w-600,cm-pad_resize"
+        />
+        <img onclick="sign()"
+          id="minimalist" 
+          src="https://images-static.nykaa.com/uploads/46d76e63-f9c8-4f0a-adb5-05ea1db740ad.jpg?tr=w-600,cm-pad_resize"
+        />
+      </div>
+    </div>
+
+
+    <div id="fifdays">
+      <img
+        src="https://images-static.nykaa.com/uploads/2825aaac-e3c4-416f-afed-f4d2f70139d0.jpg?tr=w-1200,cm-pad_resize"
+      />
+    </div>
+    <div>
+      <h2 id="spot">ONLY AT NYKAA</h2>
+      <div class="skincare">
+        <img onclick="sign()"
+          id="minimalist" 
+          src="https://images-static.nykaa.com/uploads/823cbee0-0cfe-41e9-8a2f-4b3295ac61ee.jpg?tr=w-600,cm-pad_resize"
+        />
+        <img onclick="sign()"
+          id="minimalist" 
+          src="https://images-static.nykaa.com/uploads/bb34aa79-d224-41f0-bbb6-b1cd9060a12a.gif?tr=w-600,cm-pad_resize"
+        />
+        <img
+          id="minimalist"
+          src="https://images-static.nykaa.com/uploads/eb757ee9-352c-44b1-99cf-e17ff667d286.gif?tr=w-600,cm-pad_resize"
+        />
+        <img
+          id="minimalist"
+          src="https://images-static.nykaa.com/uploads/b7b07757-d5db-45c6-bbaf-fda8ca2a0d58.jpg?tr=w-600,cm-pad_resize"
+        />
+      </div>
+    </div>
+
+
+    <div>
+      <h2 id="spot">TRENDING STORES AND GUIDES</h2>
+      <div class="skincare">
+        <img onclick="sign()"
+          id= "minimalist"
+          class="fitwidth" 
+          src="https://images-static.nykaa.com/uploads/db973daa-a107-4a90-9fe4-6d14e184565c.jpg?tr=w-1200,cm-pad_resize"
+        />
+        <img onclick="sign()"
+          id= "minimalist"
+          class="fitwidth" 
+          src="https://images-static.nykaa.com/uploads/9cbdf578-d7ab-4e4c-bdf0-76bc53d22e51.gif?tr=w-1200,cm-pad_resize"
+        />
+        <img
+          id="minimalist"
+          class="fitwidth"
+          src="https://images-static.nykaa.com/uploads/1730c33c-2ce2-4722-8f4f-d84f07a7783b.jpg?tr=w-1200,cm-pad_resize"
+        />
+        <img
+          id="minimalist"
+          class="fitwidth"
+          src="https://images-static.nykaa.com/uploads/dac00fed-ecb3-4e53-af91-2811d02b8a4c.gif?tr=w-1200,cm-pad_resize"
+        />
+      </div>
+    </div>
+
+
+    <div>
+      <h2 id="spot">FEATURED BRANDS</h2>
+      <div class="skincare fourcare">
+        <img onclick="sign()"
+          id= "minimalist"
+          class="bitwidth" 
+          src="https://images-static.nykaa.com/uploads/2da39ad4-fb02-4eef-9638-b9c805e0bb88.jpg?tr=w-300,cm-pad_resize"
+        />
+        <img onclick="sign()"
+          id= "minimalist"
+          class="bitwidth" 
+          src="https://images-static.nykaa.com/uploads/24fa43aa-8c8d-4ed0-bf7e-cda6967fd690.jpg?tr=w-300,cm-pad_resize"
+        />
+        <img
+          id="minimalist"
+          class="bitwidth"
+          src="https://images-static.nykaa.com/uploads/44fda83f-2e49-451d-adc3-7ae0528e5f2b.jpg?tr=w-300,cm-pad_resize"
+        />
+        <img
+          id="minimalist"
+          class="bitwidth"
+          src="https://images-static.nykaa.com/uploads/0651b3b2-79ab-406f-a9a6-4f56d2d74839.jpg?tr=w-300,cm-pad_resize"
+        />
+        <img
+          id="minimalist"
+          class="bitwidth"
+          src="https://images-static.nykaa.com/uploads/d7017d46-68a2-4b10-bc8d-ca14d3c9aa32.jpg?tr=w-600,cm-pad_resize"
+        />
+        <img
+          id="minimalist"
+          class="bitwidth"
+          src="https://images-static.nykaa.com/uploads/80a0158a-56e3-4967-84fc-52729bbad8f6.jpg?tr=w-600,cm-pad_resize"
+        />
+        <img
+          id="minimalist"
+          class="bitwidth"
+          src="https://images-static.nykaa.com/uploads/a917def1-92a9-43ea-ab33-ffef2641d388.jpg?tr=w-300,cm-pad_resize"
+        />
+        <img
+          id="minimalist"
+          class="bitwidth"
+          src="https://images-static.nykaa.com/uploads/e5e1e8aa-9c72-4d22-86e3-26f64a94f71c.jpg?tr=w-300,cm-pad_resize"
+        />
+      </div>
+    </div>
+    
+    <div id="singles">
+      <h2 id="spot">TOP CATEGORY FOR YOU</h2>
+      <img
+        src="https://images-static.nykaa.com/uploads/693384d1-c7c0-4f5a-956d-100f4adabd58.gif?tr=w-1200,cm-pad_resize"
+      />
+    </div>
+
+
+
+
+    <div>
+      <h2 id="spot">TRENDING STORES AND GUIDES</h2>
+      <div class="skincare">
+        <img onclick="sign()"
+          id= "minimalist"
+          class="fitwidth" 
+          src="https://images-static.nykaa.com/uploads/78340f2a-6ffb-48ae-a1a7-d0f293980997.gif?tr=w-600,cm-pad_resize"
+        />
+        <img onclick="sign()"
+          id= "minimalist"
+          class="fitwidth" 
+          src="https://images-static.nykaa.com/uploads/db432172-ed09-4050-a2ff-76113fb016ca.jpg?tr=w-600,cm-pad_resize"
+        />
+      </div>
+    </div>
+
+
+
+
+
+
+    <div>
+            <h2 id="spot">EDITOR'S CHOICE</h2>
+            <div class="skincare fourcare">
+              <img
+                id="minimalist"
+                class="bitwidth"
+                src="https://images-static.nykaa.com/uploads/f5a1d948-7947-4241-a08e-caac8d991c48.jpg?tr=w-1200,cm-pad_resize"
+              />
+              <img
+                id="minimalist"
+                class="bitwidth"
+                src="https://images-static.nykaa.com/uploads/9a4d3606-9aeb-4285-8db1-4918428a1c76.jpg?tr=w-1200,cm-pad_resize"
+              />
+              <img
+                id="minimalist"
+                class="bitwidth"
+                src="https://images-static.nykaa.com/uploads/455bf3cd-82d4-4b2c-ab5d-e56491edc0f1.jpg?tr=w-1200,cm-pad_resize"
+              />
+              <img
+                id="minimalist"
+                class="bitwidth"
+                src="https://images-static.nykaa.com/uploads/2f2275d3-0b85-4189-8fb3-7318ca1c3bec.jpg?tr=w-1200,cm-pad_resize"
+              />
+            </div>
+    </div>
+
+
+
+    <div>
+            <h2 id="spot">OFFERS YOU CAN'T MISS</h2>
+            <div class="skincare fourcare">
+              <img
+                id="minimalist"
+                class="bitwidth"
+                src="https://images-static.nykaa.com/uploads/2271b455-7d9d-4147-9205-11262261a931.gif?tr=w-300,cm-pad_resize"
+              />
+              <img
+                id="minimalist"
+                class="bitwidth"
+                src="https://images-static.nykaa.com/uploads/94350924-e838-445c-b810-4634fafb0ebd.jpg?tr=w-300,cm-pad_resize"
+              />
+              <img
+                id="minimalist"
+                class="bitwidth"
+                src="https://images-static.nykaa.com/uploads/6ea1c6f1-6e7a-434d-ba45-67e778d47b4b.jpg?tr=w-300,cm-pad_resize"
+              />
+              <img
+                id="minimalist"
+                class="bitwidth"
+                src="https://images-static.nykaa.com/uploads/984ae343-98a9-40b0-b457-3f9e8ffcc4f4.gif?tr=w-300,cm-pad_resize"
+              />
+              <img
+                id="minimalist"
+                class="bitwidth"
+                src="https://images-static.nykaa.com/uploads/e91d01dd-d891-4303-a2bb-56f06c7b3d2d.jpg?tr=w-300,cm-pad_resize"
+              />
+              <img
+                id="minimalist"
+                class="bitwidth"
+                src="https://images-static.nykaa.com/uploads/89c2cc8f-0221-4a9f-a3cc-f09cc99515f3.jpg?tr=w-300,cm-pad_resize"
+              />
+              <img
+                id="minimalist"
+                class="bitwidth"
+                src="https://images-static.nykaa.com/uploads/e494217f-1cf6-4a5b-a0e5-06fe46ac65d8.jpg?tr=w-300,cm-pad_resize"
+              />
+              <img
+                id="minimalist"
+                class="bitwidth"
+                src="https://images-static.nykaa.com/uploads/171a28d1-2676-435c-bc3b-ff59f474f248.jpg?tr=w-300,cm-pad_resize"
+              />
+            </div>
+          </div>
+
+    <div id="fifdays">
+      <img
+        src="https://images-static.nykaa.com/uploads/db5e31d0-7599-4413-8ea6-a5acdeaea99c.jpg?tr=w-1200,cm-pad_resize"
+      />
+    </div>
+    
+    <div id="fifdays" >
+      <img
+        src="https://images-static.nykaa.com/uploads/3df85e95-5d80-46b7-a8fb-75fd485c35ba.jpg?tr=w-1200,cm-pad_resize"
+      />
+    </div>
+    
+
+    <div id="singles">
+      
+      <img
+        src="https://images-static.nykaa.com/uploads/6c020368-7e2e-4418-a00f-69bfda3bf9a2.jpg?tr=w-1200,cm-pad_resize"
+      />
+    </div>
+
+
+    <div>
+            <h2 id="spot">POP-UP STORIES</h2>
+            <div class="skincare">
+              <img
+                id="minimalist"
+                src="https://images-static.nykaa.com/uploads/f88ad0a1-a2f4-4c7d-9ebd-971e8b09dd79.jpg?tr=w-600,cm-pad_resize"
+              />
+              <img
+                id="minimalist"
+                src="https://images-static.nykaa.com/uploads/7bff512e-75ca-4a5f-b7b8-23f1c3e6254c.jpg?tr=w-600,cm-pad_resize"
+              />
+            </div>
+        </div>
+
+
+
+
+    <div>
+            <h2 id="spot">SPECIAL CURATIONS</h2>
+            <div class="skincare">
+            <img class="beauty" id="minimalist" src="https://images-static.nykaa.com/uploads/d24871bd-eb78-49ae-b16f-e5dc3a8fcf45.png?tr=w-600,cm-pad_resize"/>
+        </div>
+        </div>
+
+        <div>
+            <h2 id="spot">BEAUTY ADVICE</h2>
+            <div class="skincare">
+              <img
+                id="minimalist"
+                src="https://images-static.nykaa.com/uploads/e6ae051f-56e7-4079-93e4-b844af6e1d49.jpg?tr=w-600,cm-pad_resize"
+              />
+              <img
+                id="minimalist"
+                src="https://images-static.nykaa.com/uploads/b60ba532-b630-4af2-b388-ae53f4dc7d36.jpg?tr=w-600,cm-pad_resize"
+              />
+            </div>
+        </div>
+        
+        <div id="singles">
+            <img  id="minimalist"
+              src="https://images-static.nykaa.com/uploads/58c1c0b0-5a0b-4614-acfc-3205e669d8a7.jpg?tr=w-1200,cm-pad_resize"
+            />
+          </div>
+
+
+
+</div>
+    )
+}
+
